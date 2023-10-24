7b4a9fd0db6da569b24f91b57acc69d2145c9449
tf292201 <tf292201@gmail.com>
2023-10-24 18:55:33 -0400

    Initial commit

---
+++ destructuring.js
@@ -0,0 +1,43 @@
+
+
+
+//1
+8, 1846
+
+//2
+Object {yearNeptuneDiscovered: 1846,  yearMarsDiscovered: 1659}
+
+//3
+Your name is Alejandro and you like purple
+Your name is Melissa and you like green
+Your name is undefined and you like green
+
+//4
+Maya
+Marisa
+Chi
+
+//5
+string  "Raindrops on roses"
+string  "whiskers on kittens"
+array [ "Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
+//6
+[10, 30, 20]
+
+//7
+const obj = {
+  numbers: {
+    a: 1,
+    b: 2
+  }
+};
+
+const {a, b} = obj.numbers;
+
+//8
+let arr = [1, 2];
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+//9
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest});
